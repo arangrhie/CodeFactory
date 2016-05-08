ead645b4066793628565bce77f2406dd880f98e1
Arang <arrhie@gmail.com>
2016-05-08 17:29:18 +0900

    ANNOVAR formatted variation file modification

---
+++ CodeFactory-Git/src/javax/arang/annovar/SummaryVariants.java
@@ -0,0 +1,26 @@
+package javax.arang.annovar;
+
+import javax.arang.IO.IOwrapper;
+import javax.arang.IO.basic.FileMaker;
+import javax.arang.IO.basic.FileReader;
+
+public class SummaryVariants extends IOwrapper {
+
+	@Override
+	public void hooker(FileReader fr, FileMaker fm) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void printHelp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
