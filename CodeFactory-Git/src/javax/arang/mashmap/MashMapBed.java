7a1d62c737ece2f5cc6f335be0613e768def6937
arangrhie <arrhie@gmail.com>
2019-06-10 23:51:57 -0400

    Define Mashmap bed

---
+++ CodeFactory-Git/src/javax/arang/mashmap/MashMapBed.java
@@ -0,0 +1,17 @@
+package javax.arang.mashmap;
+
+public class MashMapBed {
+	public static final int R_CHR = 0;
+	public static final int R_START = 1;
+	public static final int R_END = 2;
+
+	public static final int Q_CHR = 3;
+	public static final int Q_START = 4;
+	public static final int Q_END = 5;
+
+	public static final int R_IDENTITY = 7;
+	public static final int Q_STRAND = 8;
+
+	public static final int R_LEN = 9;
+	public static final int Q_LEN = 10;
+}
