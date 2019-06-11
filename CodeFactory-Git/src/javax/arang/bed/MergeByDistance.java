f4cd37426ce21a8e84f1107918bb8f5cf9b40b86
arangrhie <arrhie@gmail.com>
2019-06-10 23:36:04 -0400

    Use Double + fix logic

--- CodeFactory-Git/src/javax/arang/bed/MergeByDistance.java
+++ CodeFactory-Git/src/javax/arang/bed/MergeByDistance.java
@@ -6,23 +6,39 @@ import javax.arang.IO.basic.RegExp;
 import javax.arang.bed.util.Bed;
 
 public class MergeByDistance extends Rwrapper {
+	
+	private Double merged = 0d;
+	private boolean isQuiet = false;
+	
+	public Double getMergedLen() {
+		return merged;
+	}
 
+	public void setDistance(int dist) {
+		distance = dist;
+	}
+	
+	/***
+	 * Don't print the output stdout
+	 */
+	public void setOutQuiet() {
+		isQuiet = true;
+	}
+	
 	@Override
 	public void hooker(FileReader fr) {
 		String line;
 		String[] tokens;
 		
-		int prevStart = 0;
-		int prevEnd = 0;
 		int start = 0;
 		int end = 0;
+		merged = 0d;
 		
 		String prevContig = "";
 		String contig = "";
 		int regionStart = -1;
 		int regionEnd = -1;
 		boolean isFirst = true;
-		boolean hasMergedRegion = false;
 		
 		while (fr.hasMoreLines()) {
 			line = fr.readLine();
@@ -32,53 +48,45 @@ public class MergeByDistance extends Rwrapper {
 			start = Integer.parseInt(tokens[Bed.START]);
 			end = Integer.parseInt(tokens[Bed.END]);
 			
-			if (isFirst) {
-				prevContig = contig;
-				prevStart = start;
-				prevEnd = end;	
-				isFirst = false;
-				continue;
-			}
-			
 			if (!prevContig.equals(contig)) {
-				if (hasMergedRegion) {
-					// write down regions
-					System.out.println(contig + "\t" + regionStart + "\t" + regionEnd);
-				} else {
-					System.out.println(contig + "\t" + prevStart + "\t" + prevEnd);
+				if (! isFirst) {
+					// write down region
+					if (! isQuiet) {
+						System.out.println(prevContig + "\t" + regionStart + "\t" + regionEnd);
+					}
+					merged += (regionEnd - regionStart);
+
 				}
-				hasMergedRegion = false;
+				isFirst = false;
+				
+				// initialize
+				prevContig = contig;
+				regionStart = start;
+				regionEnd = end;
+				
+				merged = 0d;
 			} else {
-				if (start - prevEnd < distance) {
-					if (hasMergedRegion) {
-						regionEnd = end;	
-					} else {
-						hasMergedRegion = true;
-						regionStart = prevStart;
-						regionEnd = end;
-					}
+				// same contig
+				
+				// is overlapping?
+				if ( start - regionEnd < distance) {
+					regionEnd = end;
 				} else {
-					if (hasMergedRegion) {
-						// write down regions
+					if (! isQuiet ) {
 						System.out.println(contig + "\t" + regionStart + "\t" + regionEnd);
-					} else {
-						System.out.println(contig + "\t" + prevStart + "\t" + prevEnd);
 					}
-					hasMergedRegion = false;
+					merged += (regionEnd - regionStart);
+					regionStart = start;
+					regionEnd = end;
 				}
 			}
 			
-			prevContig = contig;
-			prevStart = start;
-			prevEnd = end;
 		}
 		
-		if (!isFirst && hasMergedRegion) {
-			// write down regions
-			System.out.println(contig + "\t" + regionStart + "\t" + regionEnd);
-		} else {
-			System.out.println(contig + "\t" + prevStart + "\t" + prevEnd);
+		if (! isQuiet) {
+			System.out.println(prevContig + "\t" + regionStart + "\t" + regionEnd);
 		}
+		merged += (regionEnd - regionStart);
 	}
 
 	@Override
@@ -87,7 +95,7 @@ public class MergeByDistance extends Rwrapper {
 		System.out.println("\t<in.bed>: any bed formatted file");
 		System.out.println("\t<stdout>: output bed formatted file, regions merged when closer than [distance]");
 		System.out.println("\t[distance]: integer. DEFAULT=30");
-		System.out.println("Arang Rhie, 2018-05-01. arrhie@gmail.com");
+		System.out.println("Arang Rhie, 2018-12-30. arrhie@gmail.com");
 	}
 
 	private static int distance = 30;
