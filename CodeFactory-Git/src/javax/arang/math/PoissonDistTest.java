c6fb59c14c98c7204138610fc4f8d60a2a96a0ed
arangrhie <arrhie@gmail.com>
2020-03-06 09:48:21 -0500

    few tests with poisson distribution

--- CodeFactory-Git/src/javax/arang/math/PoissonDistTest.java
+++ CodeFactory-Git/src/javax/arang/math/PoissonDistTest.java
@@ -6,16 +6,21 @@ import org.apache.commons.math3.distribution.PoissonDistribution;
 
 public class PoissonDistTest extends Wrapper {
 	
-	PoissonDistribution pd = new PoissonDistribution(26);
+	PoissonDistribution pd = new PoissonDistribution(63);
 	private void test() {
 		//System.out.println(Integer.MAX_VALUE);
 		startTiming();
 		
+		System.out.println(100*pd.probability(63));
+	
+		
+		/***
 		double sum = 0;
 		for (int i = 1; i < 10; i++) {
 			sum += pd.probability(i) * 25745942 * 2;
 		}
 		System.out.println(sum);
+		***/
 		printTiming();
 	}
 
