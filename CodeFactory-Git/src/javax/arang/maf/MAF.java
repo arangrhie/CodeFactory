130e6607a39e43893fa93f0b8ebd83008e8884ab
arangrhie <arrhie@gmail.com>
2021-05-05 09:07:44 -0400

    Define maf format

--- CodeFactory-Git/src/javax/arang/maf/MAF.java
+++ CodeFactory-Git/src/javax/arang/maf/MAF.java
@@ -2,8 +2,8 @@ package javax.arang.maf;
 
 public class MAF {
 
-	public static final short SRC = 1;	// or source chromosome, seq name
-	public static final short START = 2;
+	public static final short SRC = 1;		// or source chromosome, seq name
+	public static final short START = 2;	// 0-based
 	public static final short SIZE = 3;
 	public static final short STRAND = 4;
 	public static final short SRC_SIZE = 5;
